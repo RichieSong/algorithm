4de32db8fec1e0c8f86f6c3d815936383d1153a8
宋明 <songming@qianbao.com>
2020-08-31 13:45:44 +0800

    三数之和

--- 算法/数组/三数之和.py
+++ 算法/数组/三数之和.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 """
 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
 
@@ -15,10 +15,41 @@
 
 """
 
+
 class Solution:
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
-        解决思路： 两层循环
+        解决思路： 双指针
         """
+        lenth = len(nums)
+        if lenth < 3:
+            return []
+        nums.sort()
+        res = []
+        for i in range(lenth - 2):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            l, r = i + 1, lenth - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [-1, 0, 1, 2, -1, -4]
+    print(s.threeSum(nums))
--- 算法/数组/两数之和.py
+++ 算法/数组/两数之和.py
@@ -30,4 +30,4 @@ def twoSum1(nums, target):
 
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
-    print twoSum(nums, 9)
+    print(twoSum(nums, 9))
